1c90ffd82634dd20b99a1e2edb0ef98f1cb84566
Ic3manKraken <molap96@gmail.com>
2023-09-13 11:08:50 -0600

    Clases en Java para la práctica 3

--- Sistemas Operativos/Practica 3/Socket/Socket/ServidorMultiHilado.java
+++ Sistemas Operativos/Practica 3/Socket/Socket/ServidorMultiHilado.java
@@ -2,12 +2,11 @@
 import java.io.*;
 import java.net.*;
 
-public class ServidorMultiHilado  implements Runnable {
-
+public class ServidorMultiHilado implements Runnable {
     static final int PUERTO = 12345;
     Socket s;
-    public ServidorMultiHilado(){
 
+    public ServidorMultiHilado(){
         try{
             initServidor();
         }catch(Exception e){
@@ -16,21 +15,15 @@ public class ServidorMultiHilado  implements Runnable {
     }
 
     public ServidorMultiHilado(Socket s) {
-
         this.s=s;
-
     }
 
     //SERVIDOR
     public void initServidor() throws Exception {
-
-
         ServerSocket sc;
         Socket so;
 
-
-         try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
-
+        try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
             while(true) {
                 System.out.println("Esperando una conexion:");
                 so = serverSocket.accept();
@@ -39,13 +32,8 @@ public class ServidorMultiHilado  implements Runnable {
                 tcliente.start();
                 
                 //Inicia el socket, ahora esta esperando una conexion por parte del cliente
-
                 System.out.println("Un cliente se ha conectado.");
-
             }
-
-
-
         }
     }
 
@@ -53,41 +41,27 @@ public class ServidorMultiHilado  implements Runnable {
     public void run() {
         //Canales de entrada y salida de datos
         PrintWriter salida=null;
-
         String mensajeRecibido="";
-
         BufferedReader entrada=null;
 
         try {
             entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        
-        salida = new PrintWriter(s.getOutputStream(), true);
-        
-        System.out.println("Confirmando conexion al cliente....");
-
-        String mensajeCliente = entrada.readLine();
-        
-        System.out.println("Recibido del cliente: " + mensajeCliente);
-        
-        System.out.println(mensajeRecibido);
-
-        salida.println("Hola, Cliente! Recibí tu mensaje: " + mensajeCliente);
-
-        System.out.println("Cerrando conexion...");
-
-        s.close();
+            salida = new PrintWriter(s.getOutputStream(), true);
+            
+            System.out.println("Confirmando conexion al cliente....");
+            String mensajeCliente = entrada.readLine();
+            System.out.println("Recibido del cliente: " + mensajeCliente);
+            System.out.println(mensajeRecibido);
+            salida.println("Hola, Cliente! Recibí tu mensaje: " + mensajeCliente);
+            System.out.println("Cerrando conexion...");
+            s.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-
     }
 
-
-    public static void main(String[] args) throws IOException {
-        
-        
+    public static void main(String[] args) throws IOException {        
         ServidorMultiHilado s = new ServidorMultiHilado();
     }
 }
