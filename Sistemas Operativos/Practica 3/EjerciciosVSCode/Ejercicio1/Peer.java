74c5284313a957e1cd16e165c4d7133ce4b9a285
Ic3manKraken <molap96@gmail.com>
2023-09-28 20:16:00 -0600

    Ejercicio1-Practica3-SO

--- Sistemas Operativos/Practica 3/EjerciciosVSCode/Ejercicio1/Peer.java
+++ Sistemas Operativos/Practica 3/EjerciciosVSCode/Ejercicio1/Peer.java
@@ -6,14 +6,15 @@ import java.util.stream.IntStream;
 public class Peer {
     private static final String HOST = "localhost";
     private static final int PUERTO = 6000;
-    private static int numeroRecibido;
 
     public static void main(String[] args) throws IOException {
         int id = Integer.parseInt(args[0]);
         int TOTAL = Integer.parseInt(args[1]);
         int PUERTO_ID = PUERTO + id;
         int VECINO = ((id + 1) % TOTAL) + PUERTO;
-				int numeroGenerado = new Random().nextInt(100)+1;
+		int numeroGenerado = new Random().nextInt(100)+1;
+        int numeroRecibido
+        ;
 
         // Modo Servidor
         try (ServerSocket serverSocket = new ServerSocket(PUERTO_ID)) {
@@ -30,25 +31,26 @@ public class Peer {
             // Modo cliente
             try (Socket clientSocket = new Socket(HOST, VECINO)) {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-								boolean firstStep=true;
+				boolean firstStep=true;
                 for (int i = 0; i < TOTAL; i++) {
-									if(firstStep){
-										out.println(numeroGenerado);
-										System.out.println("Cliente[" + id + "]: Enviando número " + numeroGenerado);
-										firstStep=false;
-									}
-                    out.println(numeroRecibido);
-                    System.out.println("Cliente[" + id + "]: Enviando número " + numeroRecibido);
+					if(firstStep){
+						out.println(numeroGenerado);
+						System.out.println("Cliente[" + id + "]: Enviando número " + numeroGenerado);
+						firstStep=false;
+					}else {
+                        out.println(numeroRecibido);
+                        System.out.println("Cliente[" + id + "]: Enviando número " + numeroRecibido);
+                    }
                 }
             }
 						
-						Socket clientSocket = serverSocket.accept();
-						BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-						for (int i = 0; i < TOTAL; i++) {
-							String mensaje = in.readLine();
-							System.out.println("Servidor[" + PUERTO_ID + "]: Recibí el número " + mensaje);
-							numeroRecibido=Integer.parseInt(mensaje,10);
-					}
+			Socket clientSocket = serverSocket.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			for (int i = 0; i < TOTAL; i++) {
+				String mensaje = in.readLine();
+				System.out.println("Servidor[" + PUERTO_ID + "]: Recibí el número " + mensaje);
+				numeroRecibido=Integer.parseInt(mensaje,10);
+			}
         }
     }
 }
