671482a7135dd92dfe2e507bbb0f3f88ee34ecc6
JorgeMtz <molap96@gmail.com>
2023-09-29 22:46:08 -0600

    modificacion script

--- Sistemas Operativos/Practica 3/Ejercicios/Ejercicio2/javaScript.sh
+++ Sistemas Operativos/Practica 3/Ejercicios/Ejercicio2/javaScript.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-function menuCliente(){
-    while true
-    do
-        echo -e "\n\tMenú de cliente:"
-        echo -e "\nDeseas crear un cliente? s/n"
-        read opcion
-
-        case $opcion in
-        s)
-            echo -e "\nIniciando compilación del cliente"
-            javac Cliente.java
-
-            if [ $? -eq 0 ]; then
-                echo -e "\n\tCompilación del cliente exitosa!\n"
-            else
-                echo -e "\n\tError en compilación, intente de nuevo\n"
-                exit 1
-            fi
-
-            java Cliente
-            ;;
-        n)
-            echo "Saliendo..."
-            break
-            ;;
-        *) echo "Opción no válida"
-            ;;
-        esac
-    done
-}
-
 echo -e "\nIniciando compilación del servidor"
 javac Servidor.java
 
@@ -44,20 +13,3 @@ fi
 
 clear
 java Servidor &
-
-
-#sleep 2
-
-#menuCliente
-
-#killall java
-
-#echo "Ingresa el número de procesos..."
-#read processes
-
-#for ((i=0; i<$processes; i++))do
-#    fuser -k 600$i/tcp &>/dev/null
-#    echo "Ejecutando el programa para el proceso [$i]"
-#    java Peer $i $processes &
-#done
-
